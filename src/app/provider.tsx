0dcdc79ad114fb2e00826d1174ee919d8355fbe5
abhilash01820 <mekalaabhilash01820@gmail.com>
2025-07-16 22:59:43 +0530

    lll

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,54 +1,43 @@
-'use client'
+'use client';
 
 import { useUser } from '@clerk/nextjs';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { UserDetailContext } from '../../context/UserDetailContext';
 
+export type UsersDetail = {
+  name: string;
+  email: string;
+  credits: number;
+};
 
- export type UsersDetail={
-  name:string,
-  email:string,
-  credits:number
-}
 function Provider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const {user}=useUser();
-<<<<<<< HEAD
-  const [userDetail,setUserDetail]=useState<any>();
-
-
-  useEffect(()=>{
-    user && CreateNewUser();
-  },[user])
-=======
+  const { user } = useUser();
   const [userDetail, setUserDetail] = useState<UsersDetail | null>(null);
 
+  useEffect(() => {
+    if (user) {
+      CreateNewUser();
+    }
+  }, [user]);
 
- useEffect(() => {
-  if (user) {
-    CreateNewUser();
-  }
-}, [user]);
-
->>>>>>> c737540e6887cda3c583b5f04d14ffc2a662b00c
-
-  const CreateNewUser=async ()=>{
-    const result=await axios.post('/api/users');
+  const CreateNewUser = async () => {
+    const result = await axios.post('/api/users');
     console.log(result.data);
     setUserDetail(result.data);
-  }
+  };
+
   return (
     <div>
-      <UserDetailContext.Provider value={{userDetail,setUserDetail}} >
+      <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
         {children}
       </UserDetailContext.Provider>
     </div>
-  )
+  );
 }
 
 export default Provider;
